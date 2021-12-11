465a482c8494bbc3096221f9cd9a939af6af8c7b
toduq <toduqq@gmail.com>
2021-12-11 23:52:01 +0900

    Init

---
+++ src/main/kotlin/dev/todaka/kbatis/KbatisApplication.kt
@@ -0,0 +1,11 @@
+package dev.todaka.kbatis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KbatisApplication
+
+fun main(args: Array<String>) {
+    runApplication<KbatisApplication>(*args)
+}
---
+++ src/test/kotlin/dev/todaka/kbatis/KbatisApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.todaka.kbatis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KbatisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
